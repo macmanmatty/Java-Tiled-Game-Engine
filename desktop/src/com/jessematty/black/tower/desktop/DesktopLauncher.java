b97f4196c9bfb8e4fff3f34e54d19080a32bbf35
macmanmatty <jamatty@gmail.com>
2022-10-15 18:30:54 -0500

    added white space

--- desktop/src/com/jessematty/black/tower/desktop/DesktopLauncher.java
+++ desktop/src/com/jessematty/black/tower/desktop/DesktopLauncher.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.jessematty.black.tower.GameBaseClasses.BlackTower;
 
 public class DesktopLauncher {
+
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		new Lwjgl3Application(new BlackTower(), config);
