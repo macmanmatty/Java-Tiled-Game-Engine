ad0ffbe7419e5a70293ccd9e72db674a3bbf5b40
macmanmatty <jamatty@gmail.com>
2023-03-10 20:40:10 -0600

    added comments

--- core/src/com/com/jessematty/black/tower/Systems/Engine/EventSystem.java
+++ core/src/com/com/jessematty/black/tower/Systems/Engine/EventSystem.java
@@ -3,7 +3,7 @@ import com.jessematty.black.tower.GameBaseClasses.MapDraw;
 import com.jessematty.black.tower.Systems.GameEntitySystem;
 
 /**
- * system for a single event on the event has  acted it get removed from the Ashlely engine;
+ * system for a single event once the event has  acted it get removed from the libGDX Ashlely engine;
  *
  */
 public abstract  class EventSystem  extends GameEntitySystem {
