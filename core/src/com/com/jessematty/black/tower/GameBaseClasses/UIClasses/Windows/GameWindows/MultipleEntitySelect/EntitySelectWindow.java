320806a7078f8c149e64db1cadb7a1c8e980b647
jessematty <jamatty@gmail.com>
2023-11-01 17:52:11 -0400

    fixed UI closable window and added entity select window

--- core/src/com/com/jessematty/black/tower/GameBaseClasses/UIClasses/Windows/GameWindows/MultipleEntitySelect/EntitySelectWindow.java
+++ core/src/com/com/jessematty/black/tower/GameBaseClasses/UIClasses/Windows/GameWindows/MultipleEntitySelect/EntitySelectWindow.java
@@ -11,7 +11,6 @@ import com.jessematty.black.tower.GameBaseClasses.MapDraw;
 import com.jessematty.black.tower.GameBaseClasses.UIClasses.Groups.EntityHorizontalGroup;
 import com.jessematty.black.tower.GameBaseClasses.UIClasses.ItemTable.OnSelected;
 import com.jessematty.black.tower.GameBaseClasses.UIClasses.Windows.ClosableWindow;
-
 /**(
  * window for selecting a single entity from a list of entities
  */
@@ -28,12 +27,10 @@ public class EntitySelectWindow extends ClosableWindow implements EntitySettable
      * the selected entity
      */
    private Entity selectedEntity;
-
     /**
      * call back function to be preformed on the entity after it is selected.
      */
    private OnSelected<Entity> onSelected;
-
     /**
      * whether or not to close the window once an entity ha been picked;
      */
@@ -42,7 +39,6 @@ public class EntitySelectWindow extends ClosableWindow implements EntitySettable
         super("Pick An Entity", skin, true, false, false);
         this.entities = entities;
        this.gameAssets=gameAssets;
-        this.skin=skin;
         this.onSelected=onSelected;
         createWindow();
     }
@@ -51,10 +47,9 @@ public class EntitySelectWindow extends ClosableWindow implements EntitySettable
     }
     public void createWindow(){
         clear();
-
         Table table = new Table();
         for(final  Entity entity: entities){
-            EntityHorizontalGroup entityHorizontalGroup= new EntityHorizontalGroup(this, gameAssets, skin, entity);
+            EntityHorizontalGroup entityHorizontalGroup= new EntityHorizontalGroup(this, gameAssets, getSkin(), entity);
             table.add(entityHorizontalGroup);
             table.row();
             
@@ -63,7 +58,6 @@ public class EntitySelectWindow extends ClosableWindow implements EntitySettable
         scrollPane.setTransform(true);
         add(scrollPane).size(200, 400);
         pack();
-
     }
     @Override
     public void setEntity(Entity entity) {
@@ -79,20 +73,16 @@ public class EntitySelectWindow extends ClosableWindow implements EntitySettable
     public Entity getEntity() {
         return selectedEntity;
     }
-
     public Array<Entity> getEntities() {
         return entities;
     }
-
     public void setEntities(Array<Entity> entities) {
         this.entities = entities;
         createWindow();
     }
-
     public boolean isCloseOnSelect() {
         return closeOnSelect;
     }
-
     public void setCloseOnSelect(boolean closeOnSelect) {
         this.closeOnSelect = closeOnSelect;
     }
